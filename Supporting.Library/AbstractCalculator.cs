5b01f61d7096998b905ae3b2c2e6cf030fef931d
Aneesh Abraham <aneeshandap@gmail.com>
2020-08-09 16:20:24 -0500

    Abstract class learning

---
+++ Supporting.Library/AbstractCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supporting.Library
+{
+    public abstract class AbstractCalculator
+    {
+        public abstract void SayHello(string input);
+
+        public virtual void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Supporting.Library/Calculator.cs
+++ Supporting.Library/Calculator.cs
@@ -2,7 +2,7 @@
 
 namespace Supporting.Library
 {
-    public class Calculator : ICalculator
+    public class Calculator : AbstractCalculator, ICalculator
     {
         // CALC
         public Calculator()
@@ -11,12 +11,25 @@ namespace Supporting.Library
         }
         public int Add(int number1, int number2)
         {
+            WriteMessage("Add Operation");
             return number1 + number2;
         }
 
         public int Minus(int number1, int number2)
         {
+            WriteMessage("Minus");
             return number1 - number2;
         }
+
+        public override void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void SayHello(string input)
+        {
+            Console.WriteLine(input);
+        }
     }
+
 }
